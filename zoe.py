d3763995866406a742bfb0eba72e1baaf48660ea
Varma <79473081+jagguvarma15@users.noreply.github.com>
2025-04-16 17:05:26 -0400

    Update

--- zoe.py
+++ zoe.py
@@ -16,7 +16,7 @@ col1, col2 = st.columns([1, 6])
 with col1:
     st.image("assets/logo.png", width=80)
 with col2:
-    st.markdown("<h1 style='padding-top: 15px;'>Think Round, Inc</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='padding-top: 15px;'>Think Round, Inc.</h1>", unsafe_allow_html=True)
 
 st.subheader("Meet Zoe - Your Empathetic AI Assistant 💬")
 
