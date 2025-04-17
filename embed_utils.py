4650a44116a1a015d6fe8cfb8c27e4438b996f2f
Varma <79473081+jagguvarma15@users.noreply.github.com>
2025-04-16 21:06:40 -0400

    Update

--- embed_utils.py
+++ embed_utils.py
@@ -3,7 +3,6 @@ import pickle
 import faiss
 from openai import OpenAI
 from dotenv import load_dotenv
-from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 
