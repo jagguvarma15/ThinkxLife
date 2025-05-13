1f68c567df7f693ecce167064789fba5c86c028b
Varma <79473081+jagguvarma15@users.noreply.github.com>
2025-05-13 11:53:07 -0400

    Delete

--- ui/state.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def init_session_state():
-    if "chat_history" not in st.session_state:
-        st.session_state.chat_history = []
-    if "user_name" not in st.session_state:
-        st.session_state.user_name = ""
-    if "user_age" not in st.session_state:
-        st.session_state.user_age = ""
